805248aae5ed038745404801b40bb50758611e65
kiran-kharsel <kirankharsel@gmail.com>
2026-03-23 14:44:33 +0530

    feat: add side-bar in form-wrapper

--- src/form-wrapper/FormWrapper.jsx
+++ src/form-wrapper/FormWrapper.jsx
@@ -3,7 +3,21 @@ import './style.css'
 
 function FormWrapper() {
   return (
-    <div>FormWrapper</div>
+    <div>
+      <aside>
+        <div>
+          <div className="index active">1</div>
+          <div className="heading">
+            <p>STEP 1</p>
+            <p>your info</p>
+          </div>
+        </div>
+      </aside>
+      <main>
+        <div className="current-step"></div>
+        <div className="buttons"></div>
+      </main>
+    </div>
   )
 }
 
