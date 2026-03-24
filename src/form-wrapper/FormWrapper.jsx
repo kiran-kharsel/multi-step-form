4bec48ae187d413b70b7281e67ebaca4ac33ea08
kiran-kharsel <kirankharsel@gmail.com>
2026-03-24 09:50:45 +0530

    feat: show first step component

--- src/form-wrapper/FormWrapper.jsx
+++ src/form-wrapper/FormWrapper.jsx
@@ -2,6 +2,13 @@ import React, { useState } from 'react'
 import './style.css'
 import Step from './step'
 
+// import all step components
+// todo:: lazy load in other file
+import Step1 from './step1'
+import Step2 from './step2'
+import Step3 from './step3'
+import Step4 from './step4'
+
 
 // steps
 const STEPS = [
@@ -23,8 +30,25 @@ const STEPS = [
   },
 ]
 
+// pages 
+const Pages = {
+  step1: 1,
+  step2: 2,
+  step3: 3,
+  step4: 4,
+}
+
 function FormWrapper() {
-  const [isActive, setIsActive] = useState(false);
+  const [currentStep, setCurrentStep] = useState(Pages.step1);
+
+  const Steps = {
+    [Pages.step1]: Step1,
+    [Pages.step2]: Step2,
+    [Pages.step3]: Step3,
+    [Pages.step4]: Step4,
+  }
+
+  const CurrentComponent = Steps[currentStep]
 
   
   return (
@@ -40,7 +64,9 @@ function FormWrapper() {
         }
       </aside>
       <main>
-        <div className="current-step"></div>
+        <div className="current-step">
+          <CurrentComponent/>
+        </div>
         <div className="buttons">
           <button className='go-back'>go back</button>
           <button className='next-step'>next step</button>
