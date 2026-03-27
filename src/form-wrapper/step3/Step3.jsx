ba8339f03b9bcfab86ef357d26d98c9c3924d244
kiran-kharsel <kirankharsel@gmail.com>
2026-03-27 07:22:31 +0530

    feat: implement addon selection functionality

--- src/form-wrapper/step3/Step3.jsx
+++ src/form-wrapper/step3/Step3.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
+
+
+const AddOnsData = [
+  {
+    id: 1,
+    title: 'Online service',
+    desc: 'Access to multiplayer games',
+    price: '',
+  },
+  {
+    id: 2,
+    title: 'Larger storage',
+    desc: 'Extra 1TB cloud save',
+    price: '',
+  },
+  {
+    id: 3,
+    title: 'Customizable profile',
+    desc: 'Custom theme on your profile',
+    price: '',
+  },
+]
+
 function Step3() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id)
+  }
+
   return (
     <div className='add-ons-container'>
       <div className="add-ons-heading">
@@ -9,19 +38,45 @@ function Step3() {
         <p>Add-ons help to enhance your gaming experince</p>
       </div>
       <div className="add-ons">
-        <div className="add-on">
-          <input type="checkbox" className='checkbox'/>
-          <div>
-            <h4>online service</h4>
-            <p>Access to multiplayer games</p>
-          </div>
-          <span>+$10/yr</span>
-        </div>
-        <div className="add-on"></div>
-        <div className="add-on"></div>
+        {
+          AddOnsData.map((data) => {
+            return (
+              <AddOn 
+              key={data.id} 
+              checked={selectedId === data.id} 
+              data={data}
+              onClick={handleClick}/>
+            )
+          })
+        }
       </div>
     </div>
   )
 }
 
-export default Step3
+export default Step3;
+
+
+
+// addon component
+function AddOn({data, checked, onClick}) {
+  const {id, title, desc, price} = data;
+
+  function handleClick(){
+    console.log('ok')
+    onClick(id)
+  }
+
+  return (
+    <div
+      style={{ borderColor: checked ? 'mediumblue' : 'lightgray' }}
+      className="add-on" onClick={handleClick}>
+      <input checked={checked} readOnly type="checkbox" className='checkbox' />
+      <div>
+        <h4>{title}</h4>
+        <p>{desc}</p>
+      </div>
+      <p className='price'>+${10}/yr</p>
+    </div>
+  )
+}
