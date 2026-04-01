a700c566f13f501a44387022c4807d442cd29ef9
kiran-kharsel <kirankharsel@gmail.com>
2026-04-01 07:03:47 +0530

    chore: add thank-you icon in success-message component

--- src/form-wrapper/success/SuccessMsg.jsx
+++ src/form-wrapper/success/SuccessMsg.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './style.css'
+import successIcon from '../../assets/icon-thank-you.svg'
 
 function SuccessMsg() {
   return (
     <div className='success-message'>
-      <img src="" alt="success-image" />
+      <img src={successIcon} alt="success-image" />
       <h1>Thank you</h1>
       <p>Thanks for confirming your subscription! We hope you have fun using our platform. If you ever need support, please feel free to email us at support@loremgaming.com.</p>
     </div>
