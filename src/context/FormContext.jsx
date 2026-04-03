070ee629e26218ca49dfd665897d383baca304be
kiran-kharsel <kirankharsel@gmail.com>
2026-04-03 19:51:16 +0530

    feat: create initial state for user detail

--- src/context/FormContext.jsx
+++ src/context/FormContext.jsx
@@ -1,3 +1,16 @@
 import React, { createContext } from 'react';
 
 const FormContext = createContext();
+
+const initialState = {
+    userName: '',
+    email: '',
+    phone: '',
+    plan: {
+        id: null,
+        title: '',
+        price: 0,
+        billing: ''
+    },
+    addOns: []
+}
