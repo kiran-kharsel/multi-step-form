dfd43ad8c6c0cfa398439e9754d35a76680d969f
kiran-kharsel <kirankharsel@gmail.com>
2026-04-04 17:51:46 +0530

    feat: add FormContex with reducer based state management

--- src/context/FormContext.jsx
+++ src/context/FormContext.jsx
@@ -1,4 +1,4 @@
-import React, { createContext } from 'react';
+import React, { createContext, useContext, useReducer } from 'react';
 
 const FormContext = createContext();
 
@@ -13,4 +13,43 @@ const initialState = {
         billing: ''
     },
     addOns: []
+}
+
+
+
+function formReducer(state, action) {
+    switch (action.type) {
+        case 'UPDATE_FIELD':
+            return { ...state, [action.field]: action.value };
+        case 'UPDATE_PLAN':
+            return { ...state, plan: action.plan };
+        case 'TOGGLE_ADDON':
+            const exists = state.addOns.some(a => a.id === action.addOn.id);
+            return {
+                ...state,
+                addOns: exists
+                    ? state.addOns.filter(a => a.id !== action.addOn.id)
+                    : [...state.addOns, action.addOn]
+            };
+        case 'RESET':
+            return initialState;
+        default:
+            return state;
+
+    }
+}
+
+
+export function FormProvider({ children }) {
+    const [state, dispatch] = useReducer(formReducer, initialState)
+    return (
+        <FormContext.Provider value={{ formData: state, dispatch }}>
+            {children}
+        </FormContext.Provider>
+    )
+}
+
+// ccustom hook
+export function useForm(){
+    return useContext(FormContext)
 }
